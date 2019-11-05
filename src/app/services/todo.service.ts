cdecd4efefdf76526e2b5aad905709158dc3f14d
GuidoOffermans <guidooffermans@hotmail.com>
2019-11-05 22:03:11 +0100

    add togglecompleted

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -4,6 +4,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Todo } from '../models/Todo';
 
+const httpOptions = {
+	headers: new HttpHeaders({
+		'Content-type': 'application/json'
+	})
+};
+
 @Injectable({
 	providedIn: 'root'
 })
@@ -16,5 +22,10 @@ export class TodoService {
 		return this.http.get<Todo[]>(
 			`${this.todosUrl}${this.todosLimit}`
 		);
+	}
+
+	toggleCompleted(todo: Todo): Observable<any> {
+		const url = `${this.todosUrl}/${todo.id}`;
+		return this.http.put(url, todo, httpOptions);
 	}
 }
