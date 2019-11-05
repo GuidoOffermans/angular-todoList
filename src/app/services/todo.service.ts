ba7dc0b9619894e49bd2735954f0755957f11f5e
GuidoOffermans <guidooffermans@hotmail.com>
2019-11-05 21:54:55 +0100

    add service to get todos from http request

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,28 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Todo } from '../models/Todo';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class TodoService {
-	constructor() {}
+	todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+	todosLimit = '?_limit=5';
+	constructor(private http: HttpClient) {}
 
-	getTodos() {
-		return [
-			{
-				id: 1,
-				title: 'Todo one',
-				completed: false
-			},
-			{
-				id: 2,
-				title: 'Todo two',
-				completed: true
-			},
-			{
-				id: 3,
-				title: 'Todo three',
-				completed: false
-			}
-		];
+	getTodos(): Observable<Todo[]> {
+		return this.http.get<Todo[]>(
+			`${this.todosUrl}${this.todosLimit}`
+		);
 	}
 }
