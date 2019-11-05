fbceb9581334874baff912186d3e24b843d1701e
GuidoOffermans <guidooffermans@hotmail.com>
2019-11-05 21:40:30 +0100

    move todos to service

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TodoService } from '../../services/todo.service';
 import { Todo } from '../../models/Todo';
 
 @Component({
@@ -9,25 +10,10 @@ import { Todo } from '../../models/Todo';
 })
 export class TodosComponent implements OnInit {
 	todos: Todo[];
-	constructor() {}
+
+	constructor(private todoService: TodoService) {}
 
 	ngOnInit() {
-		this.todos = [
-			{
-				id: 1,
-				title: 'Todo one',
-				completed: false
-			},
-			{
-				id: 2,
-				title: 'Todo two',
-				completed: true
-			},
-			{
-				id: 3,
-				title: 'Todo three',
-				completed: false
-			}
-		];
+		this.todos = this.todoService.getTodos();
 	}
 }
